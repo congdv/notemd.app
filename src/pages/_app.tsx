36d795f74a276789e076133710c1fda9935ef32b
Cong Dao <congdaovan94@gmail.com>
2023-03-01 17:18:36 -0500

    Fix google analytics and husky

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import { GA_TRACKING_ID } from '@/utils/gtag'
 import useGtag from '@/utils/useGtag'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   useGtag()
@@ -12,19 +11,6 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>notemd.app</title>
         <link rel="icon" href="/favicon.ico" />
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-          `,
-          }}
-        />
       </Head>
       <Component {...pageProps} />
     </>
